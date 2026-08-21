a3eff12538c60d8b2490457f716cbbab123eb5f2
QuilsonLira <quilsonlira@gmail.com>
2026-08-21 09:44:33 -0300

    Conecta adaptador MySQL ao schema gerado

--- db/mysql.ts
+++ db/mysql.ts
@@ -1,5 +1,6 @@
 import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
+import * as schema from "./schema.mysql";
 
 const globalForMysql = globalThis as typeof globalThis & {
   __NEXIGREJA_MYSQL_POOL?: mysql.Pool;
@@ -38,5 +39,5 @@ export function getMysqlPool() {
 }
 
 export function getMysqlDb() {
-  return drizzle(getMysqlPool());
+  return drizzle(getMysqlPool(), { schema });
 }
